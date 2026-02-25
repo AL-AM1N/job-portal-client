b306d6a42a888640ccae661330cd939498a58c0d
MD. AL-AMIN <amin57.cse@gmail.com>
2026-02-26 03:34:25 +0600

    Add jobs data and load data in the client side

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import Banner from './Banner'
+import HotJobs from './HotJobs'
 
 function Home() {
+
+  const jobsPromise = fetch('http://localhost:3000/jobs').then(res => res.json());
+
   return (
     <div>
       <Banner></Banner>
+      <Suspense fallback={<h3>Loading...</h3>}>
+        <HotJobs jobsPromise = {jobsPromise}></HotJobs>
+      </Suspense>
     </div>
   )
 }
---
+++ src/pages/Home/HotJobs.jsx
@@ -0,0 +1,19 @@
+import React, { use } from 'react'
+import JobCard from '../Shared/JobCard';
+
+
+const HotJobs = ({jobsPromise}) => {
+
+    const jobs = use(jobsPromise);
+    console.log(jobs);
+
+  return (
+    <div className='grid grid-cols-1 gap-4 lg:grid-cols-3 md:grid-cols-2'>
+      {
+        jobs.map(job => <JobCard key={job._id} job={job}></JobCard>)
+      }
+    </div>
+  )
+}
+
+export default HotJobs
