60f84b9054a9060f74b9093ae300bfde2363c5c7
MD. AL-AMIN <amin57.cse@gmail.com>
2026-02-26 15:18:01 +0600

    Create Job detail Api and show detailed data

--- src/pages/JobDetails/JobDetails.jsx
+++ src/pages/JobDetails/JobDetails.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { useLoaderData } from 'react-router'
 
 const JobDetails = () => {
+
+  const {title, company} = useLoaderData();
+
   return (
     <div>
-      
+      <h3 className='text-6xl'>Job details of: {title}</h3>
+      <p>Company: {company}</p>
+      <button className='btn btn-primary'>Apply Now</button>
     </div>
   )
 }
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -6,6 +6,7 @@ import RootLayout from "../layouts/RootLayout";
 import Register from "../pages/Register/Register";
 import SignIn from "../pages/SignIn/SignIn";
 import JobDetails from "../pages/JobDetails/JobDetails";
+import PrivateRoute from "../pages/routes/PrivateRoute";
 
 const router = createBrowserRouter([
   {
@@ -19,7 +20,9 @@ const router = createBrowserRouter([
         },
         {
           path:'/jobs/:id',
-          Component:JobDetails
+          // Component:JobDetails,
+          element:<PrivateRoute><JobDetails></JobDetails></PrivateRoute>,
+          loader: ({params}) => fetch(`http://localhost:3000/jobs/${params.id}`)
         },
         {
             path:'/register',
